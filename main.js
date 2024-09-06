661d0944059bf16a9bfd6b40d0c290055ac3e15a
kendpr <real.kendpr@gmail.com>
2024-09-06 22:58:30 +0800

    upload memes from client to drive

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const uploadRouter = require("./router/uploadRouter");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(uploadRouter);
+
+app.app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
