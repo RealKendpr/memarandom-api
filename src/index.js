a4812e9006dd696e48c6a01ea5518233462f1405
kendpr <119304897+RealKendpr@users.noreply.github.com>
2024-09-07 14:20:51 +0800

    Combined main.js and index.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const getRouter = require("./router/getRouter");
+const uploadRouter = require("./router/uploadRouter");
 
 const app = express();
 const expressPORT = 1111;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(getRouter);
+app.use(uploadRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(expressPORT, () => {
   console.log("listening");
