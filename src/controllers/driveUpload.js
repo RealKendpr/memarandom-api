32ec0433cd7d5d8d13b5eade7bbb9036e8c5eee2
kendpr <real.kendpr@gmail.com>
2024-09-08 13:34:56 +0800

    check if the file is an image and is less than 2mb

--- src/controllers/driveUpload.js
+++ src/controllers/driveUpload.js
@@ -47,10 +47,26 @@ const uploadToDrive = async (fileObj) => {
 };
 
 const handleUpload = async (req, res) => {
+  const allowedMimeTypes = [
+    "image/png",
+    "image/jpeg",
+    "image/jpg",
+    "image/webp",
+  ];
+  const maxSize = 2 * 1024 * 1024; //bytes
+
   try {
     const { body, files } = req;
-    for (let file = 0; file < files.length; file++) {
-      await uploadToDrive(files[file]);
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+
+      if (allowedMimeTypes.includes(file.mimetype) && file.size <= maxSize) {
+        await uploadToDrive(file);
+      } else {
+        return res
+          .status(400)
+          .send(`File not allowed: ${file.size} bytes, ${file.mimetype}`);
+      }
     }
     res.status(200).send("Done");
   } catch (file) {
