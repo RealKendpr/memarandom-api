5c71ff8f0ce962569e1e303184693af7648e6eba
kendpr <119304897+RealKendpr@users.noreply.github.com>
2024-09-09 20:42:10 +0800

    sepated the login authentication from the router and made a getCollections helper to be reused

--- src/router/loginRouter.js
+++ src/router/loginRouter.js
@@ -1,40 +1,8 @@
-const connect = require("../database/database");
 const express = require("express");
+const { loginController } = require("../controllers/loginContoller");
+
 const loginRouter = express.Router();
-const bcrypt = require("bcrypt");
 
-loginRouter.post("/login", async (req, res) => {
-  // const hashedPassword = await bcrypt.hash('yourPasswordInString');
-  // console.log("hased: " + hashedPassword);
-
-  let mongoClient;
-  const { username, password } = req.body;
-
-  try {
-    mongoClient = await connect();
-
-    const collection = mongoClient
-      .db(process.env.DB_NAME)
-      .collection(process.env.LOGIN_COLLECTIONS);
-
-    const userCredential = await collection.find().toArray();
-
-    const verifyPass = await bcrypt.compare(
-      password,
-      userCredential[0].password
-    );
-
-    if (username === userCredential[0].username && verifyPass) {
-      req.session.userid = await bcrypt.hash(
-        username + "fdkjf4ng9/gjg].mgjerh13fj",
-        5
-      );
-      console.log(req.session);
-      return res.status(200).redirect("/upload");
-    } else return res.status(404).send("No records found");
-  } catch (error) {
-    console.error(error);
-  }
-});
+loginRouter.post("/login", loginController);
 
 module.exports = loginRouter;
